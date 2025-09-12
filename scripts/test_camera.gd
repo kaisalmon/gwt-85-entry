37df66cfab001274a5d319f9653577ed914de529
Kai Salmon <kaisalmon@hotmail.com>
2025-09-12 23:32:43 +0100

    Kai's first work on making a scene

---
+++ scripts/test_camera.gd
@@ -0,0 +1,11 @@
+extends Camera3D
+
+var initial_angle_y = 0.0
+var t = 0.0
+
+func _ready():
+    initial_angle_y = rotation.y
+
+func _process(delta):
+    t += delta * 0.5
+    rotation.y = initial_angle_y + sin(t) * 1
