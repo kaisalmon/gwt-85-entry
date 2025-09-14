5a17b5c1ed796af73175a92ee643b75ca81a1e5e
Substain <Dav.d@gmx.de>
2025-09-14 10:30:38 +0200

    add font, remove errors after door unlock

--- scenes/interactable/room_expander.gd
+++ scenes/interactable/room_expander.gd
@@ -5,33 +5,41 @@ var animation_progress: float = 0.0
 var rotation_speed: float = 0;
 var fired_particles: bool = false
 
-
 @export var room_type: GameState.RoomType = GameState.RoomType.BEDROOM
 @export var unlock_time: float = 7.0
+@export var star_mesh: MeshInstance3D
+@export var circle_mesh: MeshInstance3D
+@export var wall: MeshInstance3D
+@export var gpu_particles_3d: GPUParticles3D
+@export var collision_shape_3d: CollisionShape3D
+@export var doorframe: MeshInstance3D
+
+var door_unlocked: bool = false
+
 func _ready() -> void:
 	set_highlight(false)
 
 func _physics_process(delta: float) -> void:
+	if door_unlocked:
+		return
 	if animation_progress == 0.0:
 		return
 	
 	animation_progress += delta
 
-	var star = $Star
-	var circle = $Circle
-	if star != null && circle != null:
-		star.rotate_z(delta * rotation_speed)
-		circle.rotate_z(-delta * 0.5 * rotation_speed)
+	if star_mesh != null && circle_mesh != null:
+		star_mesh.rotate_z(delta * rotation_speed)
+		circle_mesh.rotate_z(-delta * 0.5 * rotation_speed)
 		rotation_speed += delta * 1
 
 		var star_scale = 1.0 + 0.025 * sin(animation_progress * 2) * animation_progress
-		star.scale = Vector3(star_scale, star_scale, star_scale)
+		star_mesh.scale = Vector3(star_scale, star_scale, star_scale)
 		var circle_scale = 1.0 + 0.025 * cos(animation_progress * 2) * animation_progress
-		circle.scale = Vector3(circle_scale, circle_scale, circle_scale)
+		circle_mesh.scale = Vector3(circle_scale, circle_scale, circle_scale)
 
-	if animation_progress >= (unlock_time - $GPUParticles3D.lifetime/2.0) && !fired_particles:
+	if animation_progress >= (unlock_time - gpu_particles_3d.lifetime/2.0) && !fired_particles:
 		fired_particles = true
-		$GPUParticles3D.emitting = true
+		gpu_particles_3d.emitting = true
 	if animation_progress >= unlock_time:
 		unlock()
 
@@ -54,13 +62,14 @@ func set_highlight(highlight_new: bool) -> void:
 
 
 func unlock() -> void:
-	if $Wall == null:
+	if door_unlocked: 
 		return
-	$Wall.queue_free()
-	$Circle.queue_free()
-	$Star.queue_free()
-	$CollisionShape3D.queue_free()
-	$Doorframe.visible = true
+	door_unlocked = true
+	wall.queue_free()
+	circle_mesh.queue_free()
+	star_mesh.queue_free()
+	collision_shape_3d.queue_free()
+	doorframe.visible = true
 	
 	var room_listeners = get_tree().get_nodes_in_group("room_listener")
 	for room_listener in room_listeners:
