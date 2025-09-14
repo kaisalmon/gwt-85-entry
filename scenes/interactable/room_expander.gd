c2722e258383fda9ef4219784c7eaba8e65a292b
Substain <Dav.d@gmx.de>
2025-09-14 10:58:23 +0200

    highlight for the door circle

--- scenes/interactable/room_expander.gd
+++ scenes/interactable/room_expander.gd
@@ -5,8 +5,15 @@ var animation_progress: float = 0.0
 var rotation_speed: float = 0;
 var fired_particles: bool = false
 
+
+
 @export var room_type: GameState.RoomType = GameState.RoomType.BEDROOM
 @export var unlock_time: float = 7.0
+@export var scale_factor: float = 0.6
+@export var deactivated_color: Color = Color.DIM_GRAY
+@export var highlight_color: Color = Color.GRAY
+@export var activated_color: Color = Color.WHITE
+@export_category("internal nodes")
 @export var star_mesh: MeshInstance3D
 @export var circle_mesh: MeshInstance3D
 @export var wall: MeshInstance3D
@@ -15,8 +22,15 @@ var fired_particles: bool = false
 @export var doorframe: MeshInstance3D
 
 var door_unlocked: bool = false
+var has_started_animation: bool = false
+
+#var default_circle_scale: Vector3
+var default_star_scale: Vector3
+var circle_mat: StandardMaterial3D
+var star_mat: StandardMaterial3D
 
 func _ready() -> void:
+	init_circle()
 	set_highlight(false)
 
 func _physics_process(delta: float) -> void:
@@ -33,7 +47,7 @@ func _physics_process(delta: float) -> void:
 		rotation_speed += delta * 1
 
 		var star_scale = 1.0 + 0.025 * sin(animation_progress * 2) * animation_progress
-		star_mesh.scale = Vector3(star_scale, star_scale, star_scale)
+		star_mesh.scale = Vector3(star_scale, star_scale, star_scale) * scale_factor
 		var circle_scale = 1.0 + 0.025 * cos(animation_progress * 2) * animation_progress
 		circle_mesh.scale = Vector3(circle_scale, circle_scale, circle_scale)
 
@@ -47,18 +61,29 @@ func can_interact(_player: Player) -> bool:
 	return animation_progress == 0.0
 
 func interact(_player: Player) -> void:
+	if has_started_animation:
+		return
+	has_started_animation = true
 	animation_progress = 0.01
+	
+	var start_anim_tween: Tween = create_tween().set_trans(Tween.TRANS_SPRING).set_parallel(true)
+	start_anim_tween.tween_property(self, "scale_factor", 1.0, 0.4)
+	start_anim_tween.tween_property(self, "circle_mat:albedo_color", activated_color, 0.4)
+	start_anim_tween.tween_property(self, "star_mat:albedo_color", activated_color, 0.4)	
+	
 	var room_listeners = get_tree().get_nodes_in_group("room_listener")
 	for room_listener in room_listeners:
 		if room_listener is RoomListener:
 			room_listener.on_room_unlock_start(room_type, unlock_time)
 
 
-	
 func set_highlight(highlight_new: bool) -> void:
-	print("highlighting for ", self.name, ": ", highlight_new)
-	#TODO
-	pass
+	if has_started_animation || door_unlocked:
+		return
+	#print("highlighting for ", self.name, ": ", highlight_new)
+	
+	star_mat.albedo_color = highlight_color if highlight_new else deactivated_color
+	circle_mat.albedo_color = highlight_color if highlight_new else deactivated_color
 
 
 func unlock() -> void:
@@ -75,3 +100,14 @@ func unlock() -> void:
 	for room_listener in room_listeners:
 		if room_listener is RoomListener:
 			room_listener.on_room_unlocked(room_type)
+
+func init_circle() -> void:
+	star_mat = star_mesh.material_override
+	circle_mat = circle_mesh.material_override
+	star_mat.albedo_color = deactivated_color
+	circle_mat.albedo_color = deactivated_color
+	default_star_scale = star_mesh.scale
+	#default_circle_scale = circle_mesh.scale
+	star_mesh.scale = default_star_scale * scale_factor
+	#circle_mesh.scale = default_circle_scale * initial_scale_factor
+	
