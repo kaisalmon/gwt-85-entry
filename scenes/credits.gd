26302a443998799e86c9f4d00c238d33e0912206
Kai Salmon <kaisalmon@hotmail.com>
2025-09-21 13:50:13 +0100

    Improved credits UX

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var all_lights_texture: Texture2D
 @export var bedroom_lights_texture: Texture2D
 @export var no_lights_texture: Texture2D
-@export var scroll_speed = 30.0
+@export var scroll_speed = 20.0
 @export var credits_control: Control
 @export var fadein_duration = 1.25
 @export var delay = 2.5
@@ -13,17 +13,16 @@ extends Node3D
 var progress = 0.0
 var end_sequence_timer = 0.0
 var speedup = 1.0
+var fadeout_timer = 0.0
+var fadeout_started = false
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _input(event: InputEvent) -> void:
+	speedup = 1.0
 	if event is InputEventMouseButton and event.pressed:
-		speedup = 5.0
-		if end_sequence_timer > 4.5:
-			get_tree().change_scene_to_file(next_scene)
-	else:
-		speedup = 1.0
+		speedup = 8.0
 
 func _process(delta: float) -> void:
 	progress += delta * speedup
@@ -33,7 +32,7 @@ func _process(delta: float) -> void:
 		return
 	credits_control.position.y -= delta * scroll_speed * speedup
 	if credits_control.position.y + credits_control.size.y < 92:
-		end_sequence_timer += delta
+		end_sequence_timer += delta * speedup
 		var house = $House
 		var material = house.get_active_material(0) as StandardMaterial3D
 		if end_sequence_timer > 1.0:
@@ -41,8 +40,11 @@ func _process(delta: float) -> void:
 		if end_sequence_timer > 2.5:
 			material.albedo_texture = no_lights_texture
 		if end_sequence_timer > 3.0:
-			if !credits_music.playing:
-				$Fadeout.color.a = lerp(0.0, 1.0, (end_sequence_timer - 3.0) / fadein_duration)
-		if end_sequence_timer > 4.5:
-			if !credits_music.playing:
+			if not fadeout_started:
+				if !credits_music.playing or speedup > 1.0:
+					fadeout_started = true
+		if fadeout_started:
+			fadeout_timer += delta
+			$Fadeout.color.a = lerp(0.0, 1.0, (fadeout_timer) / fadein_duration)
+			if fadeout_timer > fadein_duration + 1.0:
 				get_tree().change_scene_to_file(next_scene)
